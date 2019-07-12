a998447e08193f4c0a3732d776e11afb4b840262
Marco Roßberg <marco.rossberg12@gmail.com>
2019-07-12 13:33:00 +0200

    Add files via upload

---
+++ HTB/challenges/crypto/deceiful_batman_COMPLETE/getFlag.py
@@ -0,0 +1,33 @@
+Message = 'NAANAAANNNAANAAAANANANANAAAAAAAANNAANAAANAAANANNAAAAAAAANNNAANAAAAANAANAAAA'
+encMessage = Message.replace('N', 'B')
+chars = [(encMessage[i:i+5]) for i in range(0, len(encMessage), 5)]
+alphabet = {'AAAAA':'A',
+            'AAAAB':'B',
+            'AAABA':'C',
+            'AAABB':'D',
+            'AABAA':'E',
+            'AABAB':'F',
+      	    'AABBA':'G',
+            'AABBB':'H',
+            'ABAAA':'I',
+            'ABAAB':'K',
+	    'ABABA':'L',
+	    'ABABB':'M',
+	    'ABBAA':'N',
+	    'ABBAB':'O',
+	    'ABBBA':'P',
+	    'ABBBB':'Q',
+	    'BAAAA':'R',
+	    'BAAAB':'S',
+	    'BAABA':'T',
+	    'BAABB':'U/V',
+	    'BABAA':'W',
+	    'BABAB':'X',
+	    'BABBA':'Y',
+	    'BABBB':'Z'}
+
+decMessage = ''
+for char in chars:
+	if char in alphabet:
+		decMessage += alphabet[char]
+print('HTB{'+decMessage[9:]+'}')
